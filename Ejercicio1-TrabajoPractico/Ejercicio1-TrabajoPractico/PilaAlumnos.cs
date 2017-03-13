07a5725b703339d8c8ec9fc8c64b68572f50aac4
CaroLop <caro2900lopez@gmail.com>
2017-03-13 08:31:03 -0400

    TP

---
+++ Ejercicio1-TrabajoPractico/Ejercicio1-TrabajoPractico/PilaAlumnos.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_TrabajoPractico
+{
+    class Pila<T>
+    {
+        private Alumno[] Vector = new Alumno[10];
+        private int tope = -1;
+
+        public void Push(Alumno x)
+        {
+            if (!Llena())
+            {
+                tope++;
+               
+                Vector[tope] = x;
+           
+            }
+            else
+            {
+                Console.WriteLine("La pila esta llena");
+            }
+            
+        }
+        public Alumno Pop()
+        {
+
+            if (!Vacia())
+            {
+                tope--;
+                return Vector[tope + 1];
+            }
+            else
+            {
+                Console.WriteLine("La pila esta vacia");
+                return Vector[tope + 1];
+            }
+
+
+        }
+        private Boolean Vacia()
+        {
+
+            return tope == -1;
+
+        }
+        private Boolean Llena()
+        {
+
+            return tope == 10;
+
+        }
+        public void Mostar()
+        {
+            for (int i = 0; i <= tope; i++)
+            {
+                Console.Write("Nombre: "+Vector[i].Nombre1 + " Edad: "+Vector[i].Edad1+"   ");
+            }
+        }
+       
+    }
+    public class Alumno
+    {
+        string Nombre;
+        int Edad;
+        string Sexo;
+        string Carrera;
+
+        public Alumno(string nombre, int edad, string sexo, string carrera)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Sexo = sexo;
+            this.Carrera = carrera;
+        }
+
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public int Edad1 { get => Edad; set => Edad = value; }
+        public string Sexo1 { get => Sexo; set => Sexo = value; }
+        public string Carrera1 { get => Carrera; set => Carrera = value; }
+    }
+}
---
+++ Ejercicio1-TrabajoPractico/Ejercicio1-TrabajoPractico/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_TrabajoPractico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno;
+            Boolean Salir = false;
+            int opcion = 4, i=0,e;
+            string a,s,c;
+            Pila<Alumno> p = new Pila<Alumno>();
+            while (Salir == false)
+            {
+                Console.WriteLine("1.Insertar los datos del alumno");
+                Console.WriteLine("2. Eliminar ");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese los datos");
+                        Console.Write("Nombre: ");
+                        a = Console.ReadLine();
+                        Console.Write("Edad: ");
+                        e = int.Parse(Console.ReadLine());
+                        Console.Write("Sexo (M/F): ");
+                        s = Console.ReadLine();
+                        Console.Write("Carrera: ");
+                        c = Console.ReadLine();
+                        alumno = new Alumno(a, e, s,c);
+                        p.Push(alumno);
+                        i++;
+                        break;
+
+                    case 2:
+                       
+                       alumno= p.Pop();
+                        Console.WriteLine("Elemento Eliminado " +alumno.Nombre1);
+                        break;
+                    case 3:
+                        p.Mostar();
+                       
+                        Console.WriteLine();
+                        break;
+
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
