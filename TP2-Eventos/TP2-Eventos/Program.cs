e9bae94c89a2b3c07538c06ff4941b3744eaaac3
CaroLop <caro2900lopez@gmail.com>
2019-02-12 08:43:02 -0400

    Eventos2

---
+++ TP2-Eventos/TP2-Eventos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Eventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, res=0;
+            do
+            {
+                Console.WriteLine("1.-Incrementar\n2.-Disminuir\n3.-Mostrar\n4.-salir");
+                Console.WriteLine("Seleccione una opcion: ");
+                n = int.Parse(Console.ReadLine());
+                Stock st = new Stock();
+                st.Inventario += stockmin;
+                st.Stock = 10;
+                
+                switch(n)
+                {
+                    case 1:
+                        res = st.IncrementarI(5);
+                       Console.WriteLine("Incrementado a "+res);
+                        
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        
+                        Console.WriteLine("Disminuido a " + st.Disminuir(3));
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        st.Mostrar();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        break;
+                }
+                Console.WriteLine("Seleccione una opcion");
+                
+            } while (n!=4);
+            Console.ReadKey();
+        }
+        static void stockmin(int n)
+        {
+            Console.WriteLine("Alerta el Stock es minimo");
+        }
+    }
+}
---
+++ TP2-Eventos/TP2-Eventos/Stock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Eventos
+{
+    class Stock
+    {
+        private int stock;
+
+        public int Stock { get => stock; set => stock = value; }
+
+        public delegate void MDStock(int n);
+        public event MDStock Inventario;
+        public int IncrementarI(int n)
+        {
+            int sum = 0;
+            Stock += sum;
+        if(Stock<=5 && Inventario!=null)
+            {      
+                Inventario(sum);
+            }
+            return Stock+n;
+        }
+        public int Disminuir(int n)
+        {
+            int res = 0;
+            Stock -= res;
+            if (n > Stock)
+            {
+                Console.WriteLine("La disminucion es menor al stock");
+                if (Stock <= 5 && Inventario != null)
+                {
+                    Inventario(res);
+                }
+            }
+            return Stock-n;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Stock: "+Stock);
+        }
+
+    }
+}
