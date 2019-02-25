e41228c9549d74a3ada6fe291518df17c5366458
CaroLop <caro2900lopez@gmail.com>
2019-02-25 09:22:59 -0400

    Metodo seleccion

---
+++ MetodoSeleccion/MetodoSeleccion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoSeleccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = {5,2,7,2,6,1,8,3 };
+            int n = x.Length;
+            int minimo = 0,temp;
+            for (int i = 0; i < n ; i++)
+            {
+                minimo = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (x[minimo] > x[j]) minimo = j;
+                }
+                temp = x[minimo];
+                x[minimo] = x[i];
+                x[i] = temp;
+                Console.WriteLine(x[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
