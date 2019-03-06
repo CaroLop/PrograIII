9b95c3a92fb27754c0dae710182e2cf85b445d7e
CaroLop <caro2900lopez@gmail.com>
2019-03-05 20:19:11 -0500

    ListaCola

---
+++ Ej-ColaList/Ej-ColaList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_ColaList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean Salir = false;
+            int opcion = 4, a = 0;
+            Cola<int> P = new Cola<int>();
+            while(Salir==false)
+            {
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+                opcion = int.Parse(Console.ReadLine());
+                switch(opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Introduzca un numero");
+                        a = int.Parse(Console.ReadLine());
+                        P.Push(a);
+                        break;
+                    case 2:
+                        a = P.Pop();
+                        Console.WriteLine("Eliminado: " + a);
+                        break;
+                    case 3:
+                        P.Mostrar();
+                        Console.WriteLine();
+                        break;
+                }
+                
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
